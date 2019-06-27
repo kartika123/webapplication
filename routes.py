3690ffc04951fd2c40ffcd62cf0a2da6bd807164
kartika <44114312+kartika123@users.noreply.github.com>
2019-06-26 21:42:15 -0700

    Add files via upload

--- routes.py
+++ routes.py
@@ -1,12 +1,8 @@
-from flask import render_template
-from app import app
-@app.route('/')
-@app.route('/index')
-def index():
-	user = {'username': 'Kartika'}
-	posts = [{'author': {'username': 'Karan'},'body': 'Beautiful day in Patiala!'},
-	{'author': {'username': 'Muskaan'},'body': 'The Avengers movie was so cool!'
-	}
-	]
-	return render_template('index.html', title='Home',
-user=user, posts=posts)
+from flask import render_template, flash, redirect
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+	return redirect('/index')
+	return render_template('login.html', title='Sign In', form=form)
